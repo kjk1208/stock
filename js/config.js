62a1bb2c85d6852c06cf6d493566fa3cbf0e2e5f
Junkwang Kim <kjk1208@dgist.ac.kr>
2025-02-21 17:03:32 +0900

    config.js 삭제 및 gitignore 변경

--- js/config.js
+++
@@ -1,5 +0,0 @@
-// js/config.js
-const API_TOKEN = 'cuq66l1r01qviv3jd21gcuq66l1r01qviv3jd220'; // 실제 토큰으로 교체
-const GOOGLE_API_KEY = 'AIzaSyCT3aqy-g94pp8V3izmi5405UVD42BG9Kc';           // Google API Key (Calendar API)
-const GOOGLE_CALENDAR_ID = 'rlawnsrhkd@gmail.com'; // Google Calendar ID
-const PASSWORD = 'skungk'
